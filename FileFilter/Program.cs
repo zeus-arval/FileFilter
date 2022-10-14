7ee73b1fbe016cbba52360f7be0a6f26827d9715
Artur Valdna <artur.valdna@playtech.com>
2022-10-14 12:48:38 +0300

    resolved some issues

--- FileFilter/Program.cs
+++ FileFilter/Program.cs
@@ -10,7 +10,9 @@ namespace FileFilter
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
